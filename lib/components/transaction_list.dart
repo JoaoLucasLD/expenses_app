3d1ee84fc624e65225aaa3f65cbb557c77b2331d
João Lucas <joaolucaslopesd@gmail.com>
2022-04-05 10:25:22 -0300

    Implementando responsividade

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -13,68 +13,81 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 553,
-      child: transactions.isEmpty
-          ? Column(
-              children: [
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  'Nenhuma Transação Cadastrada',
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                SizedBox(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
+    return transactions.isEmpty
+        ? LayoutBuilder(
+            builder: (ctx, constraints) {
+              return Column(
+                children: [
+                  SizedBox(
+                    height: constraints.maxHeight * 0.05,
                   ),
-                ),
-              ],
-            )
-          : ListView.builder(
-              itemCount: transactions.length,
-              itemBuilder: (ctx, index) {
-                final tr = transactions[index];
-                return Card(
-                  elevation: 5,
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: Theme.of(context).colorScheme.primary,
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: FittedBox(
-                            child: Text(
-                          'R\$${tr.value}',
-                          style: const TextStyle(
-                            color: Colors.white,
-                          ),
-                        )),
-                      ),
-                    ),
-                    title: Text(
-                      tr.title,
+                  Container(
+                    height: constraints.maxHeight * 0.3,
+                    child: Text(
+                      'Nenhuma Transação Cadastrada',
                       style: Theme.of(context).textTheme.headline6,
                     ),
-                    subtitle: Text((DateFormat('d MMM y').format(tr.date))),
-                    trailing: IconButton(
-                        onPressed: () => onRemove(tr.id),
-                        color: Theme.of(context).errorColor,
-                        icon: Icon(Icons.delete)),
                   ),
-                );
-              },
-            ),
-    );
+                  SizedBox(
+                    height: constraints.maxHeight * 0.05,
+                  ),
+                  SizedBox(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              );
+            },
+          )
+        : ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (ctx, index) {
+              final tr = transactions[index];
+              return Card(
+                elevation: 5,
+                margin: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: FittedBox(
+                          child: Text(
+                        'R\$${tr.value}',
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                      )),
+                    ),
+                  ),
+                  title: Text(
+                    tr.title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text((DateFormat('d MMM y').format(tr.date))),
+                  trailing: MediaQuery.of(context).size.width > 400
+                      ? TextButton.icon(
+                          onPressed: () => onRemove(tr.id),
+                          icon: const Icon(Icons.delete), 
+                          label: const Text('Excluir'),
+                          style: TextButton.styleFrom(
+                            primary: Theme.of(context).errorColor,
+                          ),
+                      )
+                      : IconButton(
+                          onPressed: () => onRemove(tr.id),
+                          color: Theme.of(context).errorColor,
+                          icon: Icon(Icons.delete)),
+                ),
+              );
+            },
+          );
   }
 }
